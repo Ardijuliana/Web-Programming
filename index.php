d8e6f536e603b8a79a0727a04872cdfb19109ecb
Ardijuliana <ardijuliana2100@gmail.com>
2021-09-19 08:33:44 +0700

    penambahan isi index.html, penambahan isi index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+class contoh_1 extends CI_Controller{
+    public function index()
+    {
+        echo"<h1> Hai Perkenalkan </h1>";
+        echo"Nama saya Ardi Juliana <br>
+        Saya tinggal di Jakarta Timur, Duren Sawit <br>
+        Saya mahasiswa Universitas Bina Sarana Informatika <br>";
+    }
+}
+?>
